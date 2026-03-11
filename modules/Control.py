cb2bf53415afc3e69933ac51f9e1f854d90632ff
Tim <Tim.k.johnsen@gmail.com>
2026-03-11 13:00:44 -0700

    Add files via upload

---
+++ modules/Control.py
@@ -0,0 +1,211 @@
+from map_tool_box.modules import Utils
+from pathlib import Path
+import numpy as np
+import random
+import copy
+import os
+
+SUCCESS_REASONS = ['Goal']
+
+# step through an entire episode with environment, generationg actions from model
+# environment is an Episodic class object in the Environment.py module
+# model can be any object that has a predict() function as defined below
+def play_episode(environment, model, save_observations=False, save_qvalues=False):
+
+    # start episode
+    observations, state = environment.start()
+
+    # initialize and log state variables
+    if save_observations: # include observations? (this is memory heavy)
+        state['observations'] = copy.deepcopy(observations)
+    states = [state]
+    
+    # iterate through each step
+    terminate = False
+    while(not terminate):
+        
+        # get action from model based on observations
+        if save_qvalues:
+            action_value, q_values = model.predict2(observations)
+        else:
+            action_value = model.predict(observations)
+    
+        # take step based on action value
+        returned_values = environment.step(action_value)
+        if len(returned_values) == 3:
+            observations, terminate, state = returned_values
+        if len(returned_values) == 4:
+            observations, reward, terminate, state = returned_values
+        if len(returned_values) == 5:
+            observations, reward, terminate, truncate, state = returned_values
+        state['action_value'] = action_value
+        if save_qvalues:
+            state['q_values'] = q_values
+
+        # log state variables
+        if save_observations: # include observations? (this is memory heavy)
+            state['observations'] = copy.deepcopy(observations)
+        states.append(state)
+
+    # end episode
+    environment.end()
+    
+    return states
+
+# step through several episodes with environment and model
+# calculates navigation accuracy
+def eval(environment, model, write_path=None, save_observations=False, print_freq=10,
+         ckpt_freq=10, goal_threshold=4, output_progress=False, start_path_idx=None, end_path_idx=None, save_qvalues=False):
+
+    # reset spawner -- sets any vars as needed to check if there are more paths to evaluate
+    environment.spawner.reset()
+
+    # keep playing a new episode while spawner has more paths to evaluate on
+    if write_path is not None and os.path.exists(write_path):
+        episodes = Utils.pickle_read(write_path)
+        accuracy = 100*np.mean([states[-1]['end'] in SUCCESS_REASONS for states in episodes])
+        if output_progress:
+            progress = f'continuing eval from file of size {len(episodes)} and accuracy {accuracy:.2f}%'
+            job_name = Utils.get_global('job_name')
+            job_note = Utils.get_global('job_note')
+            if job_name is not None:
+                Utils.update_progress(job_name, job_note + ' ' + progress)
+            print(progress)
+    else:
+        episodes = []
+
+    # set spawner index to length of episodes
+    environment.spawner.skip_to(len(episodes))
+
+    # iterate through each episode
+    while(environment.spawner.has_more_paths()):
+        n_episodes = len(episodes)
+
+        # output progress
+        if output_progress and n_episodes % print_freq == 0 and n_episodes > 0:
+            accuracy = 100*np.mean([states[-1]['end'] in SUCCESS_REASONS for states in episodes])
+            progress = f'on episode {n_episodes} with accuracy {accuracy:.2f}%'
+            job_name = Utils.get_global('job_name')
+            job_note = Utils.get_global('job_note')
+            if job_name is not None:
+                Utils.update_progress(job_name, job_note + ' ' + progress)
+            print(progress)
+
+        # checkpoint progress
+        if write_path is not None and n_episodes % ckpt_freq == 0:
+            Utils.pickle_write(write_path, episodes)
+
+        # step though single episode
+        states = play_episode(environment, model, save_observations, save_qvalues)
+
+        # aggregate results of episode
+        episodes.append(states)
+
+    # checkpoint progress
+    if write_path is not None:
+        Utils.pickle_write(write_path, episodes)
+
+    n_successes = 0
+    for episode in episodes:
+        target = episode[0]['initial_target']
+        for state in episode:
+            point = state['point']
+            distance = point.distance(target)
+            if distance <= goal_threshold:
+                n_successes += 1
+                break
+    accuracy = 100 * n_successes / len(episodes)
+    if output_progress:
+        progress = f'finished evaluations with size {len(episodes)} and accuracy {accuracy:.2f}%'
+        job_name = Utils.get_global('job_name')
+        job_note = Utils.get_global('job_note')
+        if job_name is not None:
+            Utils.update_progress(job_name, job_note + ' ' + progress)
+        print(progress)
+    
+    return accuracy, episodes
+
+# step through several episodes with environment and model
+# calculates navigation accuracy
+def eval_set(environment, model, start_path_idx, end_path_idx, write_path=None, save_observations=False, print_freq=10,
+         ckpt_freq=10, goal_threshold=4, output_progress=False):
+
+    # reset spawner -- sets any vars as needed to check if there are more paths to evaluate
+    environment.spawner.reset()
+
+    # keep playing a new episode while spawner has more paths to evaluate on
+    if write_path is not None and os.path.exists(write_path):
+        episodes = Utils.pickle_read(write_path)
+        if output_progress:
+            progress = f'continuing eval from file of size {len(episodes)}'
+            job_name = Utils.get_global('job_name')
+            job_note = Utils.get_global('job_note')
+            if job_name is not None:
+                Utils.update_progress(job_name, job_note + ' ' + progress)
+            print(progress)
+    else:
+        episodes = {}
+
+    # set spawner index to length of episodes
+    path_idx = start_path_idx + len(episodes)
+    environment.spawner.skip_to(path_idx)
+
+    # iterate through each episode
+    n_episodes = len(episodes)
+    while(path_idx < end_path_idx):
+        n_episodes = len(episodes)
+
+        # output progress
+        if output_progress and n_episodes % print_freq == 0 and n_episodes > 0:
+            progress = f'on episode {path_idx} of {end_path_idx}'
+            job_name = Utils.get_global('job_name')
+            job_note = Utils.get_global('job_note')
+            if job_name is not None:
+                Utils.update_progress(job_name, job_note + ' ' + progress)
+            print(progress)
+
+        # checkpoint progress
+        if write_path is not None and n_episodes % ckpt_freq == 0:
+            Utils.pickle_write(write_path, episodes)
+
+        # step though single episode
+        states = play_episode(environment, model, save_observations, save_qvalues)
+
+        # aggregate results of episode
+        episodes[path_idx] = states
+
+        path_idx += 1
+
+    # checkpoint progress
+    if write_path is not None:
+        Utils.pickle_write(write_path, episodes)
+
+    n_successes = 0
+    for path_idx in episodes:
+        episode = episodes[path_idx]
+        target = episode[0]['initial_target']
+        for state in episode:
+            point = state['point']
+            distance = point.distance(target)
+            if distance <= goal_threshold:
+                n_successes += 1
+                break
+    accuracy = 100 * n_successes / len(episodes)
+
+    if output_progress:
+        progress = f'finished evaluations with size {len(episodes)} and accuracy {accuracy:.2f}'
+        job_name = Utils.get_global('job_name')
+        job_note = Utils.get_global('job_note')
+        if job_name is not None:
+            Utils.update_progress(job_name, job_note + ' ' + progress)
+        print(progress)
+    
+    return accuracy, episodes
+
+# model that selects a random discrete action index
+class RandomDiscrete:
+    def __init__(self, n_actions):
+        self.n_actions = n_actions
+
+    def predict(self, observations):
+        return random.randint(0, self.n_actions-1)
---
+++ modules/SB3Wrapper.py
@@ -0,0 +1,142 @@
+from map_tool_box.modules import Environment
+from map_tool_box.modules import Observer
+from map_tool_box.modules import Actor
+from map_tool_box.modules import Model
+from pathlib import Path
+import numpy as np
+import gymnasium
+import torch
+
+# to use the third party library StableBaselines3 (SB3) model use the following wrappers
+
+class SB3Environment(gymnasium.Env, Environment.ReinforcementLearning):
+
+    # environment is one of class objects defined above
+    def __init__(self, environment, observer_sb3, actor_sb3):
+        object.__setattr__(self, 'environment', environment)
+        # even though we do not directly use the observation or action space, these fields are necesary for sb3
+        self.observation_space = observer_sb3.get_space()
+        self.action_space = actor_sb3.get_space()
+
+    # get original object attributes    
+    def __getattr__(self, name):
+        return getattr(self.environment, name)
+    # set original object attributes    
+    def __setattr__(self, name, value):
+        setattr(self.environment, name, value)
+    # delete original object attributes    
+    def __delattr__(self, name):
+        delattr(self.environment, name)
+    
+    def step(self, action_value):
+        observation, reward, terminate, state = self.environment.step(action_value)
+        truncate = False
+        if terminate:
+            self.environment.end()
+            terminator_reason = state['end']
+            if terminator_reason not in ['Goal']:
+                truncate = True
+        info = {}
+        return observation, reward, terminate, truncate, info
+        
+    def reset(self, seed=None, options=None):
+        observation = self.environment.start()
+        info = {}
+        return observation
+
+def get_array_space(observer):
+    return gymnasium.spaces.Box(low=0, high=255, shape=observer.history_shape, dtype=np.uint8)
+
+def get_vector_space(observer):
+    return gymnasium.spaces.Box(0, 1, shape=observer.history_shape, dtype=np.float32)
+
+class SB3Observer(Observer.Observer):
+    
+    def __init__(self, observer):
+        object.__setattr__(self, 'observer', observer)
+
+    # get original object attributes    
+    def __getattr__(self, name):
+        return getattr(self.observer, name)
+    # set original object attributes    
+    def __setattr__(self, name, value):
+        setattr(self.observer, name, value)
+    # delete original object attributes    
+    def __delattr__(self, name):
+        delattr(self.observer, name)
+    
+    def get_space(self):
+        gym_space = None
+        if self.observer.o_type == 'dict':
+            spaces = {}
+            for key in self.observer.observer_dict:
+                observer = self.observer.observer_dict[key]
+                if observer.o_type == 'array':
+                    spaces[key] = get_array_space(observer)
+                if observer.o_type == 'vector':
+                    spaces[key] = get_vector_space(observer)
+            gym_space = gymnasium.spaces.Dict(spaces=spaces)
+        elif self.observer.o_type == 'array':
+            gym_space = get_array_space(self.observer)
+        elif self.observer.o_type == 'vector':
+            gym_space = get_vector_space(self.observer)
+        return gym_space
+
+class SB3Actor(Actor.Actor):
+    
+    def __init__(self, actor):
+        object.__setattr__(self, 'actor', actor)
+
+    # get original object attributes    
+    def __getattr__(self, name):
+        return getattr(self.actor, name)
+    # set original object attributes    
+    def __setattr__(self, name, value):
+        setattr(self.actor, name, value)
+    # delete original object attributes    
+    def __delattr__(self, name):
+        delattr(self.actor, name)
+    
+    def get_space(self):
+        return gymnasium.spaces.Discrete(len(self.actor.actions))
+        # return gymnasium.spaces.Box(min_space, max_space) # continuous action space TODO
+
+# takes sb3 model as input (reverse wrap)
+class ModelSB3(Model.Model):
+
+    def __init__(self, sb3model):
+        object.__setattr__(self, 'sb3model', sb3model)
+
+    # get original object attributes    
+    def __getattr__(self, name):
+        return getattr(self.sb3model, name)
+    # set original object attributes    
+    def __setattr__(self, name, value):
+        setattr(self.sb3model, name, value)
+    # delete original object attributes    
+    def __delattr__(self, name):
+        delattr(self.sb3model, name)
+    
+    def checkpoint_out(self, ckpt_dir):
+        self.save(Path(ckpt_dir, 'model__model.zip'))
+
+    def save(self, file_path):
+        self.sb3model.save(file_path)
+
+    def load(file_path):
+        return Model.read_sb3_model(file_path)
+
+    def predict(self, observations):
+        action, _states = self.sb3model.predict(observations, deterministic=True)
+        return action
+
+    # includes q-values with action
+    def predict2(self, observations):
+        self.sb3model.q_net.set_training_mode(False)
+        obs_tensor, vectorized_env = self.sb3model.q_net.obs_to_tensor(observations)
+        with torch.no_grad():
+            q_values = self.sb3model.q_net(obs_tensor)
+        q_values = q_values.cpu().numpy().reshape((-1, *self.sb3model.q_net.action_space.shape))
+        # Greedy action
+        action = q_values.argmax().reshape(-1)
+        return action, q_values
